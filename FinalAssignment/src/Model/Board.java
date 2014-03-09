e0fb003895f9017f7decbeb89371f14c40536e48
clearlyspam23 <clearlyspam23@gmail.com>
2014-03-08 17:12:09 -0800

    fleshed out the model

--- FinalAssignment/src/Model/Board.java
+++ FinalAssignment/src/Model/Board.java
@@ -1,5 +1,25 @@
 package Model;
 
+import Game.GameObject;
+
 public class Board {
+	
+	private GameObject[][] grid;
+	
+	public Board(int width, int height){
+		grid = new GameObject[width][height];
+	}
+
+	public GameObject[][] getGrid() {
+		return grid;
+	}
+	
+	public GameObject getObjectAtLocation(int x, int y){
+		return grid[x][y];
+	}
+	
+	public void setObjectAtLocation(int x, int y, GameObject o){
+		grid[x][y] = o;
+	}
 
 }
--- FinalAssignment/src/Model/Model.java
+++ FinalAssignment/src/Model/Model.java
@@ -1,7 +1,35 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+	
+	private Board board;
+	private List<Message> messages;
+	
 	public Model(){
 		
+	}
+	
+	public Model(Board b){
+		board = b;
+		messages = new ArrayList<Message>();
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void addMessage(Message m) {
+		messages.add(m);
+	}
+	
+	public void clearMessages(){
+		messages.clear();
 	}
 }
