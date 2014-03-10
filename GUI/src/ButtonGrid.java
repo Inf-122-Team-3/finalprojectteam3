2641f705bdae2c65ea654dee396a815d2f014463
Steven Ov <stevenr2d2@gmail.com>
2014-03-09 23:08:25 -0700

    GUI initial test

---
+++ GUI/src/ButtonGrid.java
@@ -0,0 +1,37 @@
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class ButtonGrid {
+
+	JFrame frame = new JFrame();
+	JButton[][] grid;
+	
+	public ButtonGrid(int width, int length){
+		frame.setLayout(new GridLayout(width,length));
+		
+		grid = new JButton[width][length];
+		for(int y = 0; y < length; y++){
+			for (int x = 0; x < width; x++){
+				grid[x][y] = new JButton("(" + x + "," + y +")");
+				frame.add(grid[x][y]);
+			}
+		}
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();frame.setVisible(true);
+		
+		for(int y = 0; y < length; y++){
+			for (int x = 0; x < width; x++){
+				grid[x][y] = new JButton("(" + x + "," + y +")");
+				frame.add(grid[x][y]);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		new ButtonGrid(3,3);
+	}
+}
