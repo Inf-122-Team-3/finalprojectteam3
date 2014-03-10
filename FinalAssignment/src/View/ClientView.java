6e658537959639fca78d04837ac089627b245ca5
Richmond Chang <changrk@uci.edu>
2014-03-09 23:22:37 -0700

    Created a very rough game view and a skeleton for the client view.

---
+++ FinalAssignment/src/View/ClientView.java
@@ -0,0 +1,31 @@
+package View;
+
+public class ClientView {
+
+	public ClientView()
+	{
+		
+	}
+	
+	private void setup()
+	{
+		//Shibani/Steven, if you have something already set up that's fine; not really sure what to do here
+		//There should be a logon window - it should have a field and a button
+		//the button should have a listener and pass the field to the Client to ask the server
+		//Then it should move the lobby
+		//Then it should ask for a list of games
+		//Then it should ask for a list of players
+		//The list of games should be a dropdown
+		//The player could just be a text field
+		//Send both the game name and the person to the server
+		//Then it should open up the gameView if its successful
+		
+		//We may have to add additional classes, I just created a dummy class here.  Thanks :D
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
